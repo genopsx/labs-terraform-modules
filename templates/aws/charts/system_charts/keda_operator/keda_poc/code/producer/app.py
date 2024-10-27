e9bfea05b235ea1cb34495e6cf61796961146d07
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-10-27 17:35:26 +0100

    ci: fix tflint-ignore: terraform_unused_declarations

--- templates/aws/charts/system_charts/keda_operator/keda_poc/code/producer/app.py
+++ templates/aws/charts/system_charts/keda_operator/keda_poc/code/producer/app.py
@@ -4,16 +4,17 @@ import boto3
 import datetime
 
 # Setting up AWS SQS client and retrieving the queue URL
-sqs = boto3.client('sqs', region_name='eu-west-1')
+sqs = boto3.client("sqs", region_name="eu-west-1")
 sqs_queue_url = os.getenv("SQS_QUEUE_URL")
+
 
 def get_queue_length():
     """Get the approximate number of messages in the SQS queue."""
     attrs = sqs.get_queue_attributes(
-        QueueUrl=sqs_queue_url,
-        AttributeNames=['ApproximateNumberOfMessages']
+        QueueUrl=sqs_queue_url, AttributeNames=["ApproximateNumberOfMessages"]
     )
-    return int(attrs['Attributes']['ApproximateNumberOfMessages'])
+    return int(attrs["Attributes"]["ApproximateNumberOfMessages"])
+
 
 def send_messages():
     """Produce messages at varying rates and send them to SQS."""
@@ -23,13 +24,15 @@ def send_messages():
     while True:
         # Stop after producing 150 messages
         if sent_count >= 150:
-            print(f"[{datetime.datetime.now()}] Producer --> Reached 150 messages, stopping production.")
+            print(
+                f"[{datetime.datetime.now()}] Producer --> Reached 150 messages, stopping production."
+            )
             break
 
         print(f"[{datetime.datetime.now()}] Producer --> Sending message to SQS")
         sqs.send_message(
             QueueUrl=sqs_queue_url,
-            MessageBody=f'Producer --> Hello from the producer! {sent_count}'
+            MessageBody=f"Producer --> Hello from the producer! {sent_count}",
         )
         sent_count += 1
 
@@ -44,6 +47,7 @@ def send_messages():
             delay = 1.0
 
         time.sleep(delay)
+
 
 if __name__ == "__main__":
     # Run the producer application standalone
