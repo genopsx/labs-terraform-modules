7ca8fb38a6852a30924661e00145c12819d6a8ad
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-10-31 20:18:42 +0100

    fix: update acm_r53

--- templates/aws/eks/addons/versions.tf
+++ templates/aws/eks/addons/versions.tf
@@ -13,9 +13,16 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.27.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.1.0"
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
     }
   }
 }
+
+
+
