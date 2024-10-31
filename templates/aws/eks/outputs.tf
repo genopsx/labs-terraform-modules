c6a4b720a584abc5751fae05155bb1478392fd93
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-10-31 16:22:51 +0100

    fix: update acm_r53

--- templates/aws/eks/outputs.tf
+++ templates/aws/eks/outputs.tf
@@ -44,14 +44,15 @@ output "eks_certificate" {
   value       = module.eks.cluster_certificate_authority_data
 }
 
-output "eks_token" {
-  description = "Authorization token for EKS K8S cluster"
-  value       = data.aws_eks_cluster_auth.eks.token
-  sensitive   = true
-}
+# output "eks_token" {
+#   description = "Authorization token for EKS K8S cluster"
+#   value       = data.aws_eks_cluster_auth.eks.token
+#   sensitive   = true
+# }
 
 output "eks_iam_role_arn" {
-  value = module.eks.cluster_iam_role_arn
+  description = "Cluster IAM role"
+  value       = module.eks.cluster_iam_role_arn
 }
 
 output "eks_oidc_issuer_url" {
@@ -116,4 +117,141 @@ output "ebs_kms_key_arn" {
 output "eks_region" {
   description = "Region where the cluster deployed"
   value       = data.aws_region.current.name
+}
+
+##################################################
+
+# output "cluster_certificate_authority_data" {
+#   description = "Cluster certificate authority data"
+#   value       = module.eks.cluster_certificate_authority_data
+# }
+
+# output "cluster_arn" {
+#   description = "Cluster ARN"
+#   value       = module.eks.cluster_arn
+# }
+
+# output "cluster_iam_role_arn" {
+#   description = "Cluster IAM role"
+#   value       = module.eks.cluster_iam_role_arn
+# }
+
+# output "cluster_endpoint" {
+#   description = "Cluster endpoint"
+#   value       = module.eks.cluster_endpoint
+# }
+
+output "cluster_iam_role_name" {
+  description = "Cluster IAM role name"
+  value       = module.eks.cluster_iam_role_name
+}
+
+# output "cluster_id" {
+#   description = "Cluster ID"
+#   value       = module.eks.cluster_id
+# }
+
+output "cluster_identity_providers" {
+  description = "Cluster identity providers"
+  value       = module.eks.cluster_identity_providers
+}
+
+output "cluster_ip_family" {
+  description = "Cluster IP family"
+  value       = module.eks.cluster_ip_family
+}
+
+# output "cluster_name" {
+#   description = "Cluster name"
+#   value       = module.eks.cluster_name
+# }
+
+# output "cluster_oidc_issuer_url" {
+#   description = "Cluster OIDC issuer URL"
+#   value       = module.eks.cluster_oidc_issuer_url
+# }
+
+output "cluster_platform_version" {
+  description = "Cluster platform version"
+  value       = module.eks.cluster_platform_version
+}
+
+output "cluster_primary_security_group_id" {
+  description = "Cluster primary security group ID"
+  value       = module.eks.cluster_primary_security_group_id
+}
+
+output "cluster_service_cidr" {
+  description = "Cluster service CIDR"
+  value       = module.eks.cluster_service_cidr
+}
+
+output "cluster_status" {
+  description = "Cluster status"
+  value       = module.eks.cluster_status
+}
+
+output "cluster_version" {
+  description = "Cluster version"
+  value       = module.eks.cluster_version
+}
+
+output "kms_key_arn" {
+  description = "KMS key ARN"
+  value       = module.eks.kms_key_arn
+}
+
+output "kms_key_id" {
+  description = "KMS key ID"
+  value       = module.eks.kms_key_id
+}
+
+output "kms_key_policy" {
+  description = "KMS key policy"
+  value       = module.eks.kms_key_policy
+}
+
+output "node_security_group_arn" {
+  description = "Node security group ARN"
+  value       = module.eks.node_security_group_arn
+}
+
+output "oidc_provider" {
+  description = "OIDC provider"
+  value       = module.eks.oidc_provider
+}
+
+output "oidc_provider_arn" {
+  description = "OIDC provider ARN"
+  value       = module.eks.oidc_provider_arn
+}
+
+output "cloudwatch_log_group_arn" {
+  description = "CloudWatch log group ARN"
+  value       = module.eks.cloudwatch_log_group_arn
+}
+
+output "cloudwatch_log_group_name" {
+  description = "CloudWatch log group name"
+  value       = module.eks.cloudwatch_log_group_name
+}
+
+output "eks_token" {
+  description = "Authorization token for EKS K8S cluster"
+  value       = module.eks.eks_token
+  sensitive   = true
+}
+
+output "acm_certificate_arn" {
+  value = module.eks.acm_certificate_arn
+}
+
+output "r53_zone_name" {
+  description = "Cluster Route 53 zone name"
+  value       = module.eks.r53_zone_name
+}
+
+output "r53_zone_id" {
+  description = "Cluster Route 53 zone ID"
+  value       = module.eks.r53_zone_id
 }
