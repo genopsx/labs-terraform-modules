e6f21aa40169e8de46930eab2f233428fb8ae4e6
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-10-31 09:57:15 +0100

    fix: update acm_r53

--- templates/aws/eks/acm_r53.tf
+++ templates/aws/eks/acm_r53.tf
@@ -4,4 +4,5 @@ module "acm_r53" {
   r53_hosted_zone_name = var.r53_hosted_zone_name
   r53_subzone_name     = var.r53_subzone_name
   cluster_name         = var.cluster_name
+  depends_on           = [module.eks]
 }
