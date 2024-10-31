ac2fedd92eada78eaa82bd7f91a3211ad50162fb
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-10-31 14:51:02 +0100

    fix: update acm_r53

--- templates/aws/eks/cluster/data.tf
+++ templates/aws/eks/cluster/data.tf
@@ -3,10 +3,12 @@ data "aws_eks_cluster_auth" "eks" {
   name = var.cluster_name
 }
 
+# tflint-ignore: terraform_unused_declarations
 data "aws_route53_zone" "parent_hosted_zone" {
   name = var.r53_hosted_zone_name
 }
 
+# tflint-ignore: terraform_unused_declarations
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {} # used for accesing Account ID and ARN
--- templates/aws/eks/cluster/locals.tf
+++ templates/aws/eks/cluster/locals.tf
@@ -1,4 +1,5 @@
 locals {
+  # tflint-ignore: terraform_unused_declarations
   fqdn = "${coalesce(var.r53_subzone_name, var.cluster_name)}.${var.r53_hosted_zone_name}"
   # tflint-ignore: terraform_unused_declarations
   private = "private"
