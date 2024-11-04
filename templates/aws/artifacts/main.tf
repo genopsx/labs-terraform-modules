62499ddcecdafa9a8f414cd0ee0dc81a0dd36f58
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-11-04 20:32:34 +0100

    feat: new modules

--- templates/aws/artifacts/main.tf
+++ templates/aws/artifacts/main.tf
@@ -1,4 +1,4 @@
 module "source_code" {
-  source          = "s3::https://s3-eu-central-1.amazonaws.com/if2-source-code-eu-central-1-720257828372/tags/acs-image-factory-terraform-modules/v0.6.1.zip//source-code-bucket"
+  source          = "./source_code_bucket"
   resource_prefix = var.resource_prefix
 }
