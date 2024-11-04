c115ac20c90a55ce74a89d3b17d7a2c36807fe13
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-11-04 16:16:36 +0100

    feat: new modules

--- templates/aws/initials/iac_pipeline/github-codebuild-ci/versions.tf
+++ templates/aws/initials/iac_pipeline/github-codebuild-ci/versions.tf
@@ -1,9 +1,13 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.70"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
     }
   }
-  required_version = "~> 1.5.5"
 }
