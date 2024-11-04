d75e51cc62b84f4e2b70f6ef9522790d0ad44a68
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-11-04 16:13:14 +0100

    feat: new modules

--- templates/aws/initials/backend.tf
+++ templates/aws/initials/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "if2-pipeline-states-eu-central-1-720257828372"
-    dynamodb_table = "if2-pipeline-locks-eu-central-1-720257828372"
-    kms_key_id     = "alias/if2-pipeline-stateskey-eu-central-1-720257828372"
-    region         = "eu-central-1"
+    bucket         = "deployment-states-terragrunt-sbx-eu-west-1-955769636964"
+    dynamodb_table = "deployment-locks-terragrunt-sbx-eu-west-1-955769636964"
+    kms_key_id     = "arn:aws:kms:eu-west-1:955769636964:key/53e2108b-0481-4fcb-8b7e-9f1ed1d25b8f"
+    region         = "eu-west-1"
     key            = "state"
   }
 }
