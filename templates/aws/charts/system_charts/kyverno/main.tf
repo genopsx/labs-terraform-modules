9853c28457d9015709ee19222d3b30ff2eeeb734
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-11-08 15:11:53 +0100

    feat: kyverno

--- templates/aws/charts/system_charts/kyverno/main.tf
+++ templates/aws/charts/system_charts/kyverno/main.tf
@@ -1,7 +1,11 @@
 # https://github.com/kyverno/kyverno
+resource "helm_repository" "kyverno" {
+  name = "kyverno"
+  url  = "https://kyverno.github.io/kyverno/"
+}
 resource "helm_release" "kyverno" {
   name       = var.kyverno_release_name
-  repository = "https://kyverno.github.io/kyverno/"
+  repository = helm_repository.kyverno.url
   chart      = var.kyverno_chart_name
   #   version          = var.kyverno_chart_version
   create_namespace = true
@@ -39,7 +43,7 @@ resource "helm_release" "kyverno" {
 resource "helm_release" "kyverno_policies" {
   depends_on = [helm_release.kyverno]
   name       = var.kyverno_policies_release_name
-  repository = "https://kyverno.github.io/kyverno/"
+  repository = helm_repository.kyverno.url
   chart      = "kyverno-policies"
   #   version    = var.kyverno_policies_chart_version
   namespace = var.kyverno_namespace # Make sure to deploy in the same namespace as Kyverno
