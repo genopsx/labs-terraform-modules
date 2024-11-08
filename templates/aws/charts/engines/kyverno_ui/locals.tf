c212601edcad9d319464dbc5bd22b9e70d439f01
Assassin010 <gauthier.kwatatshey@gmail.com>
2024-11-08 22:39:02 +0100

    feat: kyverno

--- templates/aws/charts/engines/kyverno_ui/locals.tf
+++ templates/aws/charts/engines/kyverno_ui/locals.tf
@@ -1,6 +1,6 @@
 locals {
   # Kyverno UI 
-  base_values = templatefile("${path.module}/policy_reporter/values.yaml", {
+  base_values = templatefile("${path.module}/policy_reporter/copy.yaml", {
     cert_arn                     = var.certificate_arn
     domain                       = var.domain_name
     policy_reporter_release_name = var.policy_reporter_release_name
