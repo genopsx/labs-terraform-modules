e95c73c82823ca7e8699a6d4836707b1699773dd
Gauthier Kwatatshey <95050185+Assassin010@users.noreply.github.com>
2024-12-13 04:15:04 +0100

    Update locals.tf

--- templates/aws/charts/system_charts/kyverno/locals.tf
+++ templates/aws/charts/system_charts/kyverno/locals.tf
@@ -9,8 +9,14 @@ locals {
   kyverno_secret_clone_policy_yaml_path = "${path.module}/policies/sync_secrets.yaml"
   kyverno_secret_clone_policy_yaml      = templatefile(local.kyverno_secret_clone_policy_yaml_path, {})
 
+  # Kyverno Cluster Policy
+  kyverno_cluster_policy_yaml_path      = "${path.module}/policies/kyverno_cluster_wide_psa.yaml"
+  kyverno_cluster_policy_yaml           = templatefile(local.kyverno_cluster_policy_yaml_path, {
+    cluster_version                     = var.cluster_version
+  })
+
   # Kyverno Additional Configurations
-  kyverno_secret_clone_config_yamls_path = fileset("${path.module}/additional_configs/secret_clone", "*.yaml")
+  kyverno_secret_clone_config_yamls_path = fileset("${path.module}/additional_configs/secrets_clone", "*.yaml")
   vars                                   = {}
 
   # Kyverno More policyies (to be deployed by argocd)
