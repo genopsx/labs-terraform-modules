0ba4f78a2aa44588219c8358b3387f2182e6390d
Gauthier Kwatatshey <95050185+Assassin010@users.noreply.github.com>
2024-12-13 05:01:17 +0100

    Update versions.tf

--- templates/aws/charts/system_charts/kyverno/versions.tf
+++ templates/aws/charts/system_charts/kyverno/versions.tf
@@ -7,7 +7,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.8.0"
+      version = ">= 2.6.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
