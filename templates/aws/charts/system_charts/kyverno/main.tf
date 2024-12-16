029e78b4cc804d26cad9038207d9447f33817e67
Gauthier Kwatatshey <95050185+Assassin010@users.noreply.github.com>
2024-12-16 03:08:56 +0100

    Update main.tf

--- templates/aws/charts/system_charts/kyverno/main.tf
+++ templates/aws/charts/system_charts/kyverno/main.tf
@@ -30,3 +30,51 @@ resource "helm_release" "kyverno" {
   // Omitting namespace to install Kyverno cluster-wide
   // namespace = "kyverno"
 }
+
+
+resource "helm_release" "kyverno_policies" {
+  depends_on = [helm_release.kyverno]
+  name       = var.kyverno_policies_release_name
+  repository = "https://kyverno.github.io/kyverno/"
+  chart      = var.kyverno_policies_chart_name
+  version    = var.kyverno_policies_chart_version
+  namespace  = var.kyverno_namespace # Make sure to deploy in the same namespace as Kyverno
+  # Add any additional attributes or values here if needed
+}
+
+resource "kubectl_manifest" "kyverno_cluster_policy" {
+  depends_on = [helm_release.kyverno_policies]
+  yaml_body  = local.kyverno_cluster_policy_yaml
+
+  lifecycle {
+    precondition {
+      condition     = fileexists(local.kyverno_cluster_policy_yaml_path)
+      error_message = " --> Error: Failed to find '${local.kyverno_cluster_policy_yaml_path}'. Exit terraform process."
+    }
+  }
+}
+
+# By default, Kyverno is configured with minimal permissions and does not have access to security sensitive resources like Secrets. 
+# You can provide additional permissions using cluster role aggregation. 
+# The following role permits the Kyverno background-controller to create (clone) secrets.
+# https://kyverno.io/docs/introduction/quick-start/
+resource "kubectl_manifest" "secrets_clone_config" {
+  depends_on = [helm_release.kyverno_policies]
+  for_each   = { for file in local.kyverno_secret_clone_config_yamls_path : file => file }
+  yaml_body  = templatefile("${path.module}/additional_configs/secrets_clone/${each.key}", local.vars)
+}
+
+# Next, create the following Kyverno policy. 
+# The sync-secrets policy will match on any newly-created Namespace 
+# and will clone the Secret we just created earlier into that new Namespace.
+resource "kubectl_manifest" "secrets_clone_policy" {
+  depends_on = [kubectl_manifest.secrets_clone_config]
+  yaml_body  = local.kyverno_secret_clone_policy_yaml
+
+  lifecycle {
+    precondition {
+      condition     = fileexists(local.kyverno_secret_clone_policy_yaml_path)
+      error_message = " --> Error: Failed to find '${local.kyverno_secret_clone_policy_yaml_path}'. Exit terraform process."
+    }
+  }
+}
