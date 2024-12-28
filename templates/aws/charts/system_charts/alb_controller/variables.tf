bc152e789f4f69c87fd52ddf971c0e0f87185900
Gauthier Kwatatshey <95050185+Assassin010@users.noreply.github.com>
2024-12-28 22:02:45 +0100

    Update variables.tf

--- templates/aws/charts/system_charts/alb_controller/variables.tf
+++ templates/aws/charts/system_charts/alb_controller/variables.tf
@@ -13,7 +13,7 @@ variable "namespace" {
 variable "chart_version" {
   type        = string
   description = "Helm chart to release"
-  default     = "1.10.0"
+  default     = "1.11.0"
 }
 
 variable "create_role_enabled" {
