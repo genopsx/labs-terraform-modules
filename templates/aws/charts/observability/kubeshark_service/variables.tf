71ca9aae851b40cf251a00f46dcef441fd257525
Assassin010 <gauthier.kwatatshey@gmail.com>
2025-01-05 16:21:21 +0100

    update_version

--- templates/aws/charts/observability/kubeshark_service/variables.tf
+++ templates/aws/charts/observability/kubeshark_service/variables.tf
@@ -12,7 +12,7 @@ variable "namespace" {
 variable "kubeshark_helm_chart_version" {
   description = "The version of the kubeshark helm chart"
   type        = string
-  default     = "52.3.38"
+  default     = "52.3.94"
 }
 
 variable "domain_name" {
