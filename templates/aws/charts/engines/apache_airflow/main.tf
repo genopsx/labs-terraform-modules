a01ffcfdfe8253a1dc56df9119d2005ce7324901
Assassin010 <gauthier.kwatatshey@gmail.com>
2025-02-09 19:39:01 +0100

    apache_airflow

--- templates/aws/charts/engines/apache_airflow/main.tf
+++ templates/aws/charts/engines/apache_airflow/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "airflow" {
   name             = var.name
   chart            = "airflow"
   repository       = "https://airflow.apache.org"
-  create_namespace = var.namespace == "default" ? false : true
+  create_namespace = var.namespace == "default" ? false : "us-east-1"
   version          = var.chart_version
   namespace        = var.namespace
   values           = [local.base_values]
