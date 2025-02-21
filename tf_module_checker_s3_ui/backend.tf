23dbddb9535b068a712af53a0980463dacef1198
Assassin010 <gauthier.kwatatshey@gmail.com>
2025-02-21 05:26:04 +0100

    add

--- tf_module_checker_s3_ui/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "genopsx-development-pocs-states"
-    key    = "tf_module_version_checker/terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
--- tf_module_checker_s3_ui/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "s3_bucket_name" {
-  type        = string
-  description = "The name of the S3 bucket"
-  default     = "tf-module-s3-ui"
-}
