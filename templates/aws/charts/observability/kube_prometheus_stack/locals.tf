8345bcf62250bff7b3ade6ed52f501f8cf416653
Assassin010 <gauthier.kwatatshey@gmail.com>
2025-03-08 18:47:20 +0100

    fix: Fix local_grafana_auths #5

--- templates/aws/charts/observability/kube_prometheus_stack/locals.tf
+++ templates/aws/charts/observability/kube_prometheus_stack/locals.tf
@@ -1,12 +1,12 @@
 locals {
-  base_values = templatefile("${path.module}/values.yaml", {
+  grafana_auths = jsondecode(data.aws_secretsmanager_secret_version.grafana_auths_version.secret_string)
 
+  base_values = templatefile("${path.module}/values.yaml", {
     prometheus_sub_release_name   = var.prometheus_sub_release_name
     grafana_sub_release_name      = var.grafana_sub_release_name
     alertmanager_sub_release_name = var.alertmanager_sub_release_name
     domain                        = var.domain_name
     cert_arn                      = var.certificate_arn
-    grafana_auths                 = jsondecode(data.aws_secretsmanager_secret_version.grafana_auths_version.secret_string)
     grafana_admin_user            = local.grafana_auths["grafana_admin_user"]
     grafana_admin_password        = local.grafana_auths["grafana_admin_password"]
   })
